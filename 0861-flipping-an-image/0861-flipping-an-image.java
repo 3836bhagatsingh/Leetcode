6aea44dbdb947ecb33ee3212370ced0503827bb0
Bhagat Singh <105511930+3836bhagatsingh@users.noreply.github.com>
2025-01-18 12:02:55 +0530

    Time: 1 ms (42.77%), Space: 44.8 MB (24.25%) - LeetHub

--- 0861-flipping-an-image/0861-flipping-an-image.java
+++ 0861-flipping-an-image/0861-flipping-an-image.java
@@ -1,22 +1,47 @@
 class Solution {
     public int[][] flipAndInvertImage(int[][] nums) {
-        int n = nums.length;
-        int[][] arr = new int[n][n];
-        int k=0;
+
+        // WITH EXTRA SPACE
+        // int n = nums.length;
+        // int[][] arr = new int[n][n];
+        // int k=0;
+        // for(int i=0;i<n;i++){
+        //     for(int j=n-1;j>=0;j--){
+        //       arr[i][k++] = nums[i][j];
+        //     }
+        //     k=0;
+        // }
+
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(arr[i][j]==0) arr[i][j]=1;
+        //         else arr[i][j] = 0;
+        //     }
+        // } 
+
+        // return arr; 
+
+
+         int n = nums.length;         
+        // WITHOUT EXTRA SPACE
         for(int i=0;i<n;i++){
-            for(int j=n-1;j>=0;j--){
-              arr[i][k++] = nums[i][j];
+            int a = 0,b = n-1;
+            while(a<=b){
+                int temp = nums[i][a];
+                nums[i][a] = nums[i][b];
+                nums[i][b] = temp;
+                a++;
+                b--;
             }
-            k=0;
         }
-
-        for(int i=0;i<n;i++){
+         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(arr[i][j]==0) arr[i][j]=1;
-                else arr[i][j] = 0;
+                if(nums[i][j]==0) nums[i][j]=1;
+                else nums[i][j] = 0;
             }
-        } 
+        }
+        return nums;
 
-        return arr;       
+
     }
 }

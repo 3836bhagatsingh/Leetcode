7c8fa44471e2dec2c28b4de757bd74163a6f9d5a
Bhagat Singh <105511930+3836bhagatsingh@users.noreply.github.com>
2025-01-18 11:16:36 +0530

    Time: 6 ms (34.14%), Space: 50.8 MB (32.66%) - LeetHub

--- 1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.java
+++ 1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.java
@@ -1,11 +1,24 @@
 class Solution {
     public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-       int c=0;
-       for(int i=0;i<items.size();i++){
-        if((ruleKey.equals("type") && items.get(i).get(0).equals(ruleValue)) ||
-         (ruleKey.equals("color") && items.get(i).get(1).equals(ruleValue)) ||
-        (ruleKey.equals("name") && items.get(i).get(2).equals(ruleValue))) c++;
-       }
-        return c;
+        // Brute force
+    //    int c=0;
+    //    for(int i=0;i<items.size();i++){
+    //     if((ruleKey.equals("type") && items.get(i).get(0).equals(ruleValue)) ||
+    //      (ruleKey.equals("color") && items.get(i).get(1).equals(ruleValue)) ||
+    //     (ruleKey.equals("name") && items.get(i).get(2).equals(ruleValue))) c++;
+    //    }
+    //     return c;
+      int res = 0;
+        
+        for(int i = 0 ;i<items.size();i++){
+            if(ruleKey.equals("type") && items.get(i).get(0).equals(ruleValue)) res++;
+            if(ruleKey.equals("color") && items.get(i).get(1).equals(ruleValue)) res++;
+            if(ruleKey.equals("name") && items.get(i).get(2).equals(ruleValue)) res++;
+        }
+        
+        return res;
+        
+
+
     }
 }

4f33bff21fe73fc008d7e20aaf8bc8dbb5ab17aa
Bhagat Singh <105511930+3836bhagatsingh@users.noreply.github.com>
2025-01-29 18:59:36 +0530

    Time: 0 ms (100%), Space: 42.2 MB (76.55%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,28 +1,43 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        ArrayList<Integer> l = new ArrayList<>();
-        int i=0,j=0;
-        while(i<m && j<n){
-            if(nums1[i]<=nums2[j]){
-                l.add(nums1[i]);
-                i++;
+        //Brute force approach
+        // ArrayList<Integer> l = new ArrayList<>();
+        // int i=0,j=0;
+        // while(i<m && j<n){
+        //     if(nums1[i]<=nums2[j]){
+        //         l.add(nums1[i]);
+        //         i++;
+        //     }
+        //     else if(nums2[j]<nums1[i]){
+        //          l.add(nums2[j]);
+        //          j++;
+        //     }
+        // }
+        // while(i<m){
+        //     l.add(nums1[i]);
+        //     i++;
+        // }
+        // while(j<n){
+        //     l.add(nums2[j]);
+        //     j++;
+        // }
+        // for(int k = 0;k<l.size();k++){
+        //     nums1[k] = l.get(k);
+        // }
+
+        // Optimized Approach
+        int i=m-1,
+            j = n-1,
+            k = m+n-1;
+        while(j>=0){
+            if( i>=0 && nums1[i]>=nums2[j] ){
+                nums1[k--] = nums1[i--];
             }
-            else if(nums2[j]<nums1[i]){
-                 l.add(nums2[j]);
-                 j++;
+            else{
+                nums1[k--] = nums2[j--];
             }
         }
-        while(i<m){
-            l.add(nums1[i]);
-            i++;
-        }
-        while(j<n){
-            l.add(nums2[j]);
-            j++;
-        }
-        for(int k = 0;k<l.size();k++){
-            nums1[k] = l.get(k);
-        }
+
         
     }
 }

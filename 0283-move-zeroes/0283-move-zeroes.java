bc284c816b2ea68b42bc93547032678ae8d7ec32
Bhagat Singh <105511930+3836bhagatsingh@users.noreply.github.com>
2025-01-29 11:53:51 +0530

    Time: 1 ms (99.72%), Space: 46.3 MB (19.04%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        if(n==0 || n==1) return;
+        
+        // Brute force approach
+        int[] arr = new int[n];
+        int k = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                arr[k++] = nums[i];
+            }
+        }
+         for(int i=0;i<n;i++){   
+                nums[i] = arr[i];   
+        }
+        
+    }
+}

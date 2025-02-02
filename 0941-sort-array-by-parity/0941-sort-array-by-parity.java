eb1f4b3a243bbf99fc233766219dd3c679f8ef00
Bhagat Singh <105511930+3836bhagatsingh@users.noreply.github.com>
2025-02-02 10:52:49 +0530

    Time: 1 ms (49.52%), Space: 44.9 MB (60.24%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.java
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.java
@@ -1,18 +1,40 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int n = nums.length;
-        int k =0;
-        int[] arr = new int[n];
-        for(int i=0;i<n;i++){
-            if(nums[i]%2==0){
-                arr[k++] = nums[i];
+        // Brute Force
+        // int n = nums.length;
+        // int k =0;
+        // int[] arr = new int[n];
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]%2==0){
+        //         arr[k++] = nums[i];
+        //     }
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]%2!=0){
+        //         arr[k++] = nums[i];
+        //     }
+        // }
+        // return arr;
+
+        //Optimized
+        int n = nums.length,i=0,j=n-1;
+        while(i<j){
+            if(nums[i]%2==0) i++;
+            else if (nums[j]%2==1){
+                j--;
             }
-        }
-        for(int i=0;i<n;i++){
-            if(nums[i]%2!=0){
-                arr[k++] = nums[i];
+            else{
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j--;
             }
+        
         }
-        return arr;
+        return nums;
+
+
+
     }
 }

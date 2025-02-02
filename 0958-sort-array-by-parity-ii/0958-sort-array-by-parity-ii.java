c282eaf5716a1561e103a5991152e50a7b3dc4cf
Bhagat Singh <105511930+3836bhagatsingh@users.noreply.github.com>
2025-02-02 11:28:17 +0530

    Time: 4 ms (15.55%), Space: 47.9 MB (12.43%) - LeetHub

--- 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.java
+++ 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.java
@@ -1,18 +1,34 @@
 class Solution {
     public int[] sortArrayByParityII(int[] nums) {
-        int n  = nums.length;
-        int[] arr = new int[n];
-        int even =0,odd =1;
-        for(int i=0;i<n;i++){
-            if(nums[i]%2==0){
-                arr[even] = nums[i];
-                even +=2;
+
+        // Brute force
+        // int n  = nums.length;
+        // int[] arr = new int[n];
+        // int even =0,odd =1;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]%2==0){
+        //         arr[even] = nums[i];
+        //         even +=2;
+        //     }
+        //     else{
+        //         arr[odd] = nums[i];
+        //         odd+=2;
+        //     }
+        // }
+        // return arr;
+
+        //optimized
+        int n = nums.length;
+        int even = 0,odd =1;
+        while(even<n && odd<n){
+            if(nums[odd]%2 < nums[even]%2){
+                int temp = nums[odd];
+                nums[odd] = nums[even];
+                nums[even] = temp;
             }
-            else{
-                arr[odd] = nums[i];
-                odd+=2;
-            }
+            if(nums[even]%2==0) even = even +2;
+            if(nums[odd]%2==1) odd = odd +2;
         }
-        return arr;
+        return nums;
     }
 }

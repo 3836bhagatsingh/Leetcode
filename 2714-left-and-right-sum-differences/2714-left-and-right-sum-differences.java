b3d30493eb7a84ce4e10cab0a76ed3b3f8054f59
Bhagat Singh <105511930+3836bhagatsingh@users.noreply.github.com>
2025-03-03 13:42:28 +0530

    Time: 2 ms (98.73%), Space: 44.7 MB (54.54%) - LeetHub

--- 2714-left-and-right-sum-differences/2714-left-and-right-sum-differences.java
+++ 2714-left-and-right-sum-differences/2714-left-and-right-sum-differences.java
@@ -1,24 +1,41 @@
 class Solution {
     public int[] leftRightDifference(int[] nums) {
         // Brute force  
+        // int n = nums.length;
+        // int[] l = new int[n];
+        // int[] r = new int[n];
+        // int[] ans = new int[n];
+
+        // l[0] = 0;
+        // for(int i=1;i<n;i++){
+        //     l[i] = l[i-1] + nums[i-1];
+        // }
+
+        // r[n-1] = 0;
+        // for(int i =n-2;i>=0;i--){
+        //     r[i] = r[i+1] + nums[i+1];
+        // }
+        // for(int i=0;i<n;i++){
+        //     ans[i] = Math.abs(l[i]-r[i]);
+        // }
+        // return ans;
+
+
+        // Optimal solution
+
         int n = nums.length;
-        int[] l = new int[n];
-        int[] r = new int[n];
-        int[] ans = new int[n];
-
-        l[0] = 0;
-        for(int i=1;i<n;i++){
-            l[i] = l[i-1] + nums[i-1];
+        int rightsum =0;
+        for(int i =0;i<n;i++){
+            rightsum = rightsum + nums[i];
         }
-
-        r[n-1] = 0;
-        for(int i =n-2;i>=0;i--){
-            r[i] = r[i+1] + nums[i+1];
+        int leftsum =0;
+        for(int i =0;i<n;i++){
+            int a = nums[i];
+            leftsum = leftsum + a;
+            nums[i] = Math.abs(rightsum-leftsum);
+            rightsum = rightsum - a;
         }
-        for(int i=0;i<n;i++){
-            ans[i] = Math.abs(l[i]-r[i]);
-        }
-        return ans;
+        return nums;
         
     }
 }

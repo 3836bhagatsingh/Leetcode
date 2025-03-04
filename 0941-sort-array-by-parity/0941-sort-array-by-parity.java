6a64b2fa5baf2abf5eebac2780ab98560ae6937f
Bhagat Singh <105511930+3836bhagatsingh@users.noreply.github.com>
2025-03-04 10:41:22 +0530

    Time: 0 ms (100%), Space: 45.5 MB (8.06%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.java
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.java
@@ -1,40 +1,17 @@
 class Solution {
+    public void swap(int[] arr,int i,int j){
+        int temp  = arr[i];
+        arr[i]  = arr[j];
+        arr[j] = temp;
+    }
     public int[] sortArrayByParity(int[] nums) {
-        // Brute Force
-        // int n = nums.length;
-        // int k =0;
-        // int[] arr = new int[n];
-        // for(int i=0;i<n;i++){
-        //     if(nums[i]%2==0){
-        //         arr[k++] = nums[i];
-        //     }
-        // }
-        // for(int i=0;i<n;i++){
-        //     if(nums[i]%2!=0){
-        //         arr[k++] = nums[i];
-        //     }
-        // }
-        // return arr;
-
-        //Optimized
-        int n = nums.length,i=0,j=n-1;
-        while(i<j){
-            if(nums[i]%2==0) i++;
-            else if (nums[j]%2==1){
-                j--;
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0){
+                swap(nums,i,k);
+                k++;
             }
-            else{
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                i++;
-                j--;
-            }
-        
         }
         return nums;
-
-
-
     }
 }

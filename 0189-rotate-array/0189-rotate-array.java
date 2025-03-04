df4573a8c78818954825b150ecfb124c1f8f10a9
Bhagat Singh <105511930+3836bhagatsingh@users.noreply.github.com>
2025-03-04 13:48:33 +0530

    Time: 1 ms (50.94%), Space: 57.3 MB (76.32%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,34 +1,18 @@
 class Solution {
-    public void rotate(int[] arr, int k) {
-        // Brute Force
-        //  int n = arr.length;
-        // k = k%n;
-        // int[] a = new int[n];
-        // for(int i=0;i<n;i++){
-        //     int index = (i+k)%n;
-        //     a[index] = arr[i];
-        // }
-        // for(int i=0;i<n;i++){
-        //     arr[i] = a[i];
-        // }
-
-        //optimized
-         int n = arr.length;
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int[] ans = new int[n];
         k = k%n;
-        reverse(arr,0,n-1);
-        reverse(arr,0,k-1);
-        reverse(arr,k,n-1);
-
-
-    }
-    static  void reverse(int[] arr,int i,int j){
-        while(i<j) {
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            i++;
-            j--;
+        int idx=0;
+        for(int i=n-k;i<n;i++){
+            ans[idx++] = nums[i]; 
         }
-
+        for(int i=0;i<n-k;i++){
+            ans[idx++] = nums[i]; 
+        }
+        for(int i=0;i<n;i++){
+         nums[i] = ans[i]; 
+        }
+        
     }
 }
